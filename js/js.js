20682b21a5dbb255d6873491c0850f6d722ca018
nixon galario <mac@macs-MacBook-Pro.local>
2016-04-10 10:25:55 +0800

    dime coin on png format

--- js/js.js
+++ js/js.js
@@ -1,36 +1,3 @@
-// $(document).ready(function() {
-//     $.preloadImages = function() {
-//         for (var i = 0; i < arguments.length; i++) {
-//             $("<img />").attr("src", arguments[i]);
-//         }
-//     }
-
-//     $.preloadImages("http://www.coinnews.net/wp-content/uploads/2015/03/2015-W-Proof-Roosevelt-Silver-Dime-Obverse.jpg", "http://www.coinnews.net/wp-content/uploads/2015/03/2015-P-Reverse-Proof-Roosevelt-Silver-Dime-Reverse.jpg");
-//     $('h1').addClass('animated fadeInDown');
-
-//     function flipMe() {
-//         var flip = ["heads", "tails"];
-//         var side = flip[Math.floor(Math.random() * flip.length)];
-
-//         if (side == "heads") {
-//             setTimeout(function(){
-//                 $("#regTitle").html("You Got " + side).addClass('animated pulse');
-//             },800);
-//             $('h1').addClass('animated pulse');
-//             $(".image").html('<img class="heads" src="http://www.coinnews.net/wp-content/uploads/2015/03/2015-W-Proof-Roosevelt-Silver-Dime-Obverse.jpg"/>');
-//             $('.heads').addClass('animated flip');
-//         } else {
-//             setTimeout(function(){
-//                 $("#regTitle").html("You Got " + side);
-//             },800);
-//             $('h1').addClass('animated pulse');
-//             $(".image").html('<img class="tails" src="http://www.coinnews.net/wp-content/uploads/2015/03/2015-P-Reverse-Proof-Roosevelt-Silver-Dime-Reverse.jpg"/>');
-//             $('.tails').addClass('animated flip');
-//         }
-//     }
-
-//     $(".butts").click(flipMe);
-// });
 
 $(window).load(function() { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
@@ -48,6 +15,7 @@ jQuery(document).ready(function($){
     if (spin == 'animation2160') {
         setTimeout(function(){
             $('.result').html('Result: Heads!');
+
         },700);
     } else {
         setTimeout(function(){
